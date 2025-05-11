699c432fce11bf341126b15ac5e66417fc3dfc6d
vivian5285 <wangjiali240@gmail.com>
2025-05-12 00:16:35 +0800

    1

---
+++ user-api/src/strategies/enums.ts
@@ -0,0 +1,11 @@
+export enum StrategyType {
+  GRID = 'GRID',
+  MARTINGALE = 'MARTINGALE',
+  CUSTOM = 'CUSTOM'
+}
+
+export enum StrategyStatus {
+  ACTIVE = 'ACTIVE',
+  PAUSED = 'PAUSED',
+  STOPPED = 'STOPPED'
+} 
