cafcf817a5d5b6f9c51af8ee6f04567c3efca9e8
vivian5285 <wangjiali240@gmail.com>
2025-05-13 00:47:38 +0800

    1

--- admin-ui/vite.config.ts
+++ admin-ui/vite.config.ts
@@ -46,6 +46,10 @@ export default defineConfig({
             return 'vendor'
           }
         },
+        format: 'es',
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
       }
     },
     chunkSizeWarningLimit: 1000,
